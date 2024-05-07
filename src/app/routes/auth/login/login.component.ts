b2f32635bb7ab6187e29072e6b5c9cb4384bcf09
danajacobc <danajacobc@gmail.com>
2024-05-07 18:30:44 -0300

    login

---
+++ src/app/routes/auth/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'auth-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styles: ``
+})
+export class LoginComponent {
+
+}
